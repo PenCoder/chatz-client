a74dfa90211f2f139bb31cae2b38d6cdd9ceaaa8
Pencoder <jajaclus3@gmail.com>
2021-06-11 11:16:08 +0000

    commit

---
+++ src/config.tsx
@@ -0,0 +1,10 @@
+import pack from '../package.json';
+
+export default {
+    development: {
+        endpoint: 'https://localhost:5001'
+    },
+    production: {
+        endpoint: 'https://chatzapi.herokuapp.com'
+    }
+}
--- src/views/chatroom/index.tsx
+++ src/views/chatroom/index.tsx
@@ -1,8 +1,9 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import React, { FormEvent, useEffect, useRef, useState } from 'react';
-// import io from "socket.io-client";
 import ChatArea from '../../components/chat_area';
+import config from '../../config';
+
 
 function ChatRoom() {
     const [hub, setHub] = useState<null | HubConnection>(null);
@@ -10,6 +11,8 @@ function ChatRoom() {
     const [users, setUsers] = useState(Array());
     const [selectedUser, setSelectedUser] = useState(null);
     console.log(window.location.hostname);
+
+    const endpoint = config.production.endpoint;
 
     const { logout, user } = useAuth0();
 
@@ -20,91 +23,96 @@ function ChatRoom() {
     const msgForm = useRef<HTMLFormElement>(null)
 
     useEffect(() => {
-        // Set Connection 
-        const con = new HubConnectionBuilder()
-            .withUrl(`https://localhost:5001/hubs/chatter`)
-            .withAutomaticReconnect()
-            .build();
+        try {
+            // Set Connection 
+            const con = new HubConnectionBuilder()
+                .withUrl(`${endpoint}/hubs/chatter`)
+                .withAutomaticReconnect()
+                .build();
 
-        con.start()
-            .then(result => {
-                console.log(result);
+            con.start()
+                .then(result => {
+                    console.log(result);
 
-                con.on('NewSignIn', async (user) => {
-                    await getUsers();
-                });
-
-                con.on('ReceiveMessage', async (sender, receiver) => {
-                    if (selectedUser) {
-                        if ((selectedUser as any).email === sender && email === receiver) {
-                            console.log('received...');
-                            getUserMessages(selectUser);
-                        }
-                    }
-                });
-
-                con.on('BlockUser', async (blocked, by) => {
-                    // alert(blocked)
-                    // alert(by)
-                    if (blocked === email) {
-                        if (selectedUser && (selectedUser as any).email === by) {
-                            setSelectedUser(null);
-                        }
-                    }
-
-                    getUsers();
-                });
-                con.on('UnBlockUser', async (blocked, by) => {
-                    getUsers();
-                });
-
-                con.on('LogoutUser', async (userEmail) => {
-                    if (email === userEmail) {
-                        logout({ returnTo: window.location.origin });
-                    }
-                    else {
-                        if (selectedUser && (selectedUser as any).email === userEmail) {
-                            setSelectedUser(null);
-                        }
+                    con.on('NewSignIn', async (user) => {
                         await getUsers();
-                    }
+                    });
+
+                    con.on('ReceiveMessage', async (sender, receiver) => {
+                        if (selectedUser) {
+                            if ((selectedUser as any).email === sender && email === receiver) {
+                                console.log('received...');
+                                getUserMessages(selectUser);
+                            }
+                        }
+                    });
+
+                    con.on('BlockUser', async (blocked, by) => {
+                        // alert(blocked)
+                        // alert(by)
+                        if (blocked === email) {
+                            if (selectedUser && (selectedUser as any).email === by) {
+                                setSelectedUser(null);
+                            }
+                        }
+
+                        getUsers();
+                    });
+                    con.on('UnBlockUser', async (blocked, by) => {
+                        getUsers();
+                    });
+
+                    con.on('LogoutUser', async (userEmail) => {
+                        if (email === userEmail) {
+                            logout({ returnTo: window.location.origin });
+                        }
+                        else {
+                            if (selectedUser && (selectedUser as any).email === userEmail) {
+                                setSelectedUser(null);
+                            }
+                            await getUsers();
+                        }
+                    });
                 });
-            });
-
-        // After User Signed In 
-        const onSignedIn = async () => {
-
-            try {
-                let response = await fetch('https://localhost:5001/api/users/signed_in', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ email, name, picture })
-                });
-                let result = await response.text();
-
-                if (result === 'success') {
-
-                    await getUsers();
-
-                    console.log(result)
-                } else {
-                    console.log(result)
-                }
-            }
-            catch (e) {
-                console.log(e);
-            }
+        } catch (error) {
+            console.log(error);
         }
+
         onSignedIn();
 
     }, [user, messages])
 
+    // After User Signed In 
+    const onSignedIn = async () => {
+
+        try {
+            let response = await fetch(`${endpoint}/api/users/signed_in`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, name, picture })
+            });
+            let result = await response.text();
+
+            if (result === 'success') {
+
+                await getUsers();
+
+                console.log(result)
+            } else {
+                console.log(result)
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+
 
     const onLogout = async () => {
         try {
-            let response = await fetch(`https://localhost:5001/api/users/logout?email=${email}`);
+            let response = await fetch(`${endpoint}/api/users/logout?email=${email}`);
             let results = await response.text();
             logout({ returnTo: window.location.origin });
         } catch (error) {
@@ -124,7 +132,7 @@ function ChatRoom() {
         const params = `sender=${email}&receiver=${(selectedUser as any).email}&message=${message}`;
 
         try {
-            var response = await fetch(`https://localhost:5001/api/message/send?` + params);
+            var response = await fetch(`${endpoint}/api/message/send?` + params);
             var results = await response.text();
             if (results === 'success') {
                 // Clear Form 
@@ -147,7 +155,7 @@ function ChatRoom() {
         try {
             let userEmail = user.email;
 
-            var response = await fetch(`https://localhost:5001/api/message/messages/?sender=${email}&receiver=${userEmail}`);
+            var response = await fetch(`${endpoint}/api/message/messages/?sender=${email}&receiver=${userEmail}`);
             var results = await response.json();
             setMessages([...results]);
         }
@@ -158,7 +166,7 @@ function ChatRoom() {
     // Block User 
     const onBlockUser = async () => {
         let userEmail = (selectedUser as any).email;
-        let response = await fetch(`https://localhost:5001/api/users/block_user?user=${userEmail}&by=${email}`);
+        let response = await fetch(`${endpoint}/api/users/block_user?user=${userEmail}&by=${email}`);
         let results = await response.text();
         if (results === 'success') {
             let user = selectedUser as any;
@@ -171,7 +179,7 @@ function ChatRoom() {
     const onUnBlockUser = async () => {
 
         let userEmail = (selectedUser as any).email;
-        let response = await fetch(`https://localhost:5001/api/users/unblock_user?user=${userEmail}&by=${email}`);
+        let response = await fetch(`${endpoint}/api/users/unblock_user?user=${userEmail}&by=${email}`);
         let results = await response.text();
         if (results === 'success') {
             let user = selectedUser as any;
@@ -185,7 +193,7 @@ function ChatRoom() {
         let onlineUsers = users;
 
         try {
-            let res = await fetch(`https://localhost:5001/api/users/load_users?email=${email}`);
+            let res = await fetch(`${endpoint}/api/users/load_users?email=${email}`);
             let results = await res.json();
 
             onlineUsers = results;
